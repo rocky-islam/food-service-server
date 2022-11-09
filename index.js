2517590eab575397aa9322a09f2d4d5390b0d428
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2022-11-09 23:12:33 +0600

    email wise query done

--- index.js
+++ index.js
@@ -59,10 +59,24 @@ async function run(){
 
 
         // review ApI
+        // review add to database
         app.post('/reviews', async(req, res) =>{
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
+        });
+
+        // get review data form database
+        app.get('/reviews', async(req, res) =>{
+            let query ={};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         })
 
     }
